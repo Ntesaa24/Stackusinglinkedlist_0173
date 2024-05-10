124650967ad12cbdf2a7578089d4c242f22886d0
Ntesaa24 <notesa.aldinasari24@gmail.com>
2024-05-11 03:29:46 +0700

    push

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -32,4 +32,14 @@ public:
 		cout << "Push value: " << value << endl; 
 		return value; 
 	}
+
+	// Pop operation: Remove the topmost element from the stack 
+	void pop() {
+		if (isEmpty()) {
+			cout << "Stack is empty." << endl; 
+		}
+		cout << "Popped value: " << top->data << endl; 
+		top = top->next; // update the top pointer to the next node
+
+	}
 };
