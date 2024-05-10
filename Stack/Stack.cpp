e903e61d1ca4bd8086eaa9c5d40aac5d3f64638f
Ntesaa24 <notesa.aldinasari24@gmail.com>
2024-05-11 03:21:36 +0700

    stack class

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -7,4 +7,27 @@ class Node {
 public: 
 	int data;
 	Node* next; 
+
+	Node() {
+		next = NULL; 
+	}
+};
+
+// stack class 
+class Stack {
+private:
+	Node* top; // Pointer to the top node of the stuck 
+
+public: 
+	Stack() {
+		top = NULL; // Initialize the stack with a null top pointer 
+	}
+
+	// Push operation Insert an element onto the top of the stack 
+	int push(int value) {
+		Node* newNode = new Node(); // 1. Allocate memorry for the new node 
+		newNode->data = value; // 2. assign value 
+		newNode->next = top; //3. Set the next pointer of the new node to current top node 
+		top = newNode; // 4. Update the top pointer to the new node 
+	}
 };
